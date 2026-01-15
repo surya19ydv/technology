cdecfbd6d1b203a18fc2322813ce7b5524ed94ac
$uryakala Yadav <100560146+surya19ydv@users.noreply.github.com>
2026-01-16 00:27:21 +0530

    Update Vite configuration for server settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
-    host: true,
-    port: 5173,
-  },
+    host: "0.0.0.0",
+    port: 3000,
+    strictPort: true,
+    hmr: {
+      host: "65.2.146.147",
+      protocol: "ws",
+      port: 3000
+    }
+  }
 });
